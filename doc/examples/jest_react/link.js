c12fe3319a9aea50a74110979bb46ccd777a37de
Annie S <annieshin1997@gmail.com>
2021-03-10 11:34:09 -0800

    attempt to remove react

--- doc/examples/jest_react/link.js
+++ doc/examples/jest_react/link.js
@@ -2,6 +2,20 @@ import React from 'react';
 
 export default class Link extends React.Component {
   render() {
-    return <a href={this.props.page || '#'}>{this.props.children}</a>;
+    return (
+    
+    // <a href={this.props.page || '#'}>{this.props.children}</a>;
+    
+    <div>
+        <h1> Hello World</h1>
+        <h1> This should be an error </h1>
+        <input type="text"></input>
+        {/* // I find this next idea very IMGtimidating */}
+        <img src="https://www.cesarsway.com/wp-content/uploads/2015/06/puppy-checklist.png" alt=""></img>
+        <button className="gabi"></button>
+        <div>this is a div</div>
+    </div>
+    
+    )
   }
 }
--- doc/examples/jest_react/link.test.js
+++ doc/examples/jest_react/link.test.js
@@ -5,20 +5,55 @@ import { mountToDoc } from './test-helpers';
 import Link from './link';
 
 test('Link has no axe violations', done => {
-  const linkComponent = mountToDoc(
-    <Link page="http://www.axe-core.org">axe website</Link>
-  );
-  const linkNode = linkComponent.getDOMNode();
+  // const linkComponent = mountToDoc(
+  //   <Link page="http://www.axe-core.org">axe website</Link>
+  // );
 
+  // // getDOMNode() --> https://enzymejs.github.io/enzyme/docs/api/ReactWrapper/getDOMNode.html
+  // const linkNode = linkComponent.getDOMNode();
+
+  // exclude tests that are incompatible
   const config = {
     rules: {
       'color-contrast': { enabled: false },
       'link-in-text-block': { enabled: false }
     }
   };
-  axe.run(linkNode, config, (err, { violations }) => {
-    expect(err).toBe(null);
-    expect(violations).toHaveLength(0);
+
+  console.log(document.querySelector('.wally'));
+
+  axe.run(document.querySelector('.wally'), config, (err, results) => {
+    // expect(err).toBe(null);
+    // expect(results.violations).toHaveLength(2);
+
+    // console.log(results.violations);
+
+    results.violations.forEach((axeViolation) => {
+      const whereItFailed = axeViolation.nodes[0].html;
+
+      const failureSummary = axeViolation.nodes[0].failureSummary;
+
+      const { description, help, helpUrl } = axeViolation;
+
+      const resultObj = {
+        description,
+        help,
+        helpUrl,
+        whereItFailed,
+        // failureSummary,
+      }
+      
+      // console.log(resultObj);
+
+      console.log('test description: ', description,
+        '\nto fix: ', help,
+        '\nmore info: ', helpUrl,
+        '\nwhere it failed: ', whereItFailed,
+        // '\nhow to fix: ', failureSummary
+      )
+    })
+    // console.log(results.violations);
+
     done();
   });
 });
