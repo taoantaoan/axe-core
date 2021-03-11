fb5547c6c88d99815bf2dc1c306e72c1c0c8af55
Tolan Thornton <taoantaoan@hotmail.com>
2021-03-11 12:25:01 -0700

    Cleaned up code comments; changed placement of expect assertions; removed non-used variables

--- doc/examples/jest_react/link.test.js
+++ doc/examples/jest_react/link.test.js
@@ -1,79 +1,55 @@
-const regeneratorRuntime = require("regenerator-runtime");
+const regeneratorRuntime = require('regenerator-runtime');
 import axe from 'axe-core';
 
 const fs = require('fs');
 const path = require('path');
-const html = fs.readFileSync(path.resolve(__dirname, './index2.html'), 'utf8');
+const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
 
-// describe('Run axe-core accessibility tests', () => {
-
-  test('html file has no axe violations', (done) => {
-  
+describe('Evaluate axe-core violations', () => {
+  it('html file has no axe violations', done => {
     // exclude tests that are incompatible
-    const config = {    
+    const config = {
       rules: {
         'color-contrast': { enabled: false },
         'link-in-text-block': { enabled: false }
       }
     };
-  
+
     document.documentElement.lang = 'en';
     document.documentElement.innerHTML = html.toString();
-    // [x] prints out the following
-    console.log(document.documentElement.innerHTML);
 
-    // jest.setTimeout(50000)
-  
     axe.run(config, async (err, { violations }) => {
-      // if (err) console.log(err);
-      // [x] prints out the following
-      console.log('violations.length:', violations.length, 'err: ', err);
-      // console.log('-----------------------------------------------------line31-----------------------------------------------');
-      expect(err).toBe(null);
-      // expect(violations).not.toHaveLength(0);
-      // console.log('-----------------------------------------------------line34-----------------------------------------------');
-      
-      // expect(1).toEqual(2);
+      if (err) console.log('err: ', err);
+
       if (violations.length === 0) {
-        console.log('Congrats! Keep up the good work, you have 0 known violations!');
-        done();
+        console.log(
+          'Congrats! Keep up the good work, you have 0 known violations!'
+        );
+      } else {
+        violations.forEach(axeViolation => {
+          console.log('-------');
+          const whereItFailed = axeViolation.nodes[0].html;
+          // const failureSummary = axeViolation.nodes[0].failureSummary;
+
+          const { description, help, helpUrl } = axeViolation;
+
+          console.log(
+            'TEST DESCRIPTION: ',
+            description,
+            '\nISSUE: ',
+            help,
+            '\nMORE INFO: ',
+            helpUrl,
+            '\nWHERE IT FAILED: ',
+            whereItFailed
+            // '\nhow to fix: ', failureSummary
+          );
+        });
       }
-      // console.log(results.violations);
-      
-      violations.forEach((axeViolation) => {
-        console.log('-----------------------------------------------------line36-----------------------------------------------');
-        const whereItFailed = axeViolation.nodes[0].html;
-  
-        // [x] prints out the following
-        const failureSummary = axeViolation.nodes[0].failureSummary;
-  
-        const { description, help, helpUrl } = axeViolation;
-  
-        const resultObj = {
-          description,
-          help,
-          helpUrl,
-          whereItFailed,
-          // failureSummary,
-        }
-        
-        // console.log(resultObj);
-  
-        console.log('TEST DESCRIPTION: ', description,
-          '\nISSUE: ', help,
-          '\nMORE INFO: ', helpUrl,
-          '\nWHERE IT FAILED: ', whereItFailed,
-          // '\nhow to fix: ', failureSummary
-        )
-      })
-      
-      // console.log(results.violations);
+
+      expect(err).toBe(null);
+      expect(violations).toHaveLength(0);
       done();
     });
   });
-  
-
-// })
-
-
-
+});
