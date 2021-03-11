a48241e719dea4e88b820231d1c02e34f3d6f73f
Annie S <annieshin1997@gmail.com>
2021-03-10 16:57:34 -0800

    trying to fix async

--- doc/examples/jest_react/link.test.js
+++ doc/examples/jest_react/link.test.js
@@ -1,63 +1,69 @@
-import React from 'react';
 import axe from 'axe-core';
-import { mountToDoc } from './test-helpers';
 
 const fs = require('fs');
 const path = require('path');
 const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
 
-import Link from './link';
+describe('Run axe-core accessibility tests', () => {
 
-test('Link has no axe violations', done => {
-  // const linkComponent = mountToDoc(
-  //   <Link page="http://www.axe-core.org">axe website</Link>
-  // );
-
-  // // getDOMNode() --> https://enzymejs.github.io/enzyme/docs/api/ReactWrapper/getDOMNode.html
-  // const linkNode = linkComponent.getDOMNode();
-
-  // exclude tests that are incompatible
-  const config = {
-    rules: {
-      'color-contrast': { enabled: false },
-      'link-in-text-block': { enabled: false }
-    }
-  };
-
-  document.documentElement.innerHTML = html.toString()
-
-  axe.run(config, (err, results) => {
-    // expect(err).toBe(null);
-    // expect(results.violations).toHaveLength(2);
-
-    // console.log(results.violations);
-
-    results.violations.forEach((axeViolation) => {
-      const whereItFailed = axeViolation.nodes[0].html;
-
-      const failureSummary = axeViolation.nodes[0].failureSummary;
-
-      const { description, help, helpUrl } = axeViolation;
-
-      const resultObj = {
-        description,
-        help,
-        helpUrl,
-        whereItFailed,
-        // failureSummary,
+  it('html file has no axe violations', (done) => {
+  
+    // exclude tests that are incompatible
+    const config = {    
+      rules: {
+        'color-contrast': { enabled: false },
+        'link-in-text-block': { enabled: false }
       }
-      
-      // console.log(resultObj);
-
-      console.log('TEST DESCRIPTION: ', description,
-        '\nISSUE: ', help,
-        '\nMORE INFO: ', helpUrl,
-        '\nWHERE IT FAILED: ', whereItFailed,
-        // '\nhow to fix: ', failureSummary
-      )
-    })
-    // console.log(results.violations);
-
-    done();
+    };
+  
+    document.documentElement.innerHTML = html.toString();
+    // [x] prints out the following
+    console.log(document.documentElement.innerHTML);
+    // jest.setTimeout(50000)
+  
+    axe.run(config, (err, results) => {
+      expect(err).toBe(null);
+      // [x] prints out the following
+      console.log('results.violations.length:', results.violations.length, 'err: ', err);
+      console.log('-----------------------------------------------------line31-----------------------------------------------');
+      // expect(results.violations).toHaveLength(0);
+      expect(1).toEqual(2);
+  
+      // console.log(results.violations);
+      console.log('-----------------------------------------------------line34-----------------------------------------------');
+      results.violations.forEach((axeViolation) => {
+        console.log('-----------------------------------------------------line36-----------------------------------------------');
+        const whereItFailed = axeViolation.nodes[0].html;
+  
+        // [x] prints out the following
+        const failureSummary = axeViolation.nodes[0].failureSummary;
+  
+        const { description, help, helpUrl } = axeViolation;
+  
+        const resultObj = {
+          description,
+          help,
+          helpUrl,
+          whereItFailed,
+          // failureSummary,
+        }
+        
+        // console.log(resultObj);
+  
+        console.log('TEST DESCRIPTION: ', description,
+          '\nISSUE: ', help,
+          '\nMORE INFO: ', helpUrl,
+          '\nWHERE IT FAILED: ', whereItFailed,
+          // '\nhow to fix: ', failureSummary
+        )
+      })
+      // console.log(results.violations);
+      done();
+    });
   });
-});
+  
+
+})
+
+
+
