b078537f4a3d321d7120ffba944075eaf39cc0f2
Annie S <annieshin1997@gmail.com>
2021-03-10 22:26:05 -0800

    remove 'expect' statement, replacing with 'if' and console displays

--- doc/examples/jest_react/link.test.js
+++ doc/examples/jest_react/link.test.js
@@ -3,7 +3,7 @@ import axe from 'axe-core';
 
 const fs = require('fs');
 const path = require('path');
-const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
+const html = fs.readFileSync(path.resolve(__dirname, './index2.html'), 'utf8');
 
 // describe('Run axe-core accessibility tests', () => {
 
@@ -17,51 +17,58 @@ const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
       }
     };
   
+    document.documentElement.lang = 'en';
     document.documentElement.innerHTML = html.toString();
     // [x] prints out the following
     console.log(document.documentElement.innerHTML);
+
     // jest.setTimeout(50000)
   
     axe.run(config, async (err, { violations }) => {
       // if (err) console.log(err);
       // [x] prints out the following
       console.log('violations.length:', violations.length, 'err: ', err);
-      console.log('-----------------------------------------------------line31-----------------------------------------------');
+      // console.log('-----------------------------------------------------line31-----------------------------------------------');
       expect(err).toBe(null);
-      expect(violations).toHaveLength(0);
-      console.log('-----------------------------------------------------line34-----------------------------------------------');
-      done();
+      // expect(violations).not.toHaveLength(0);
+      // console.log('-----------------------------------------------------line34-----------------------------------------------');
+      
       // expect(1).toEqual(2);
-  
-      // console.log(results.violations);
-      // results.violations.forEach((axeViolation) => {
-      //   console.log('-----------------------------------------------------line36-----------------------------------------------');
-      //   const whereItFailed = axeViolation.nodes[0].html;
-  
-      //   // [x] prints out the following
-      //   const failureSummary = axeViolation.nodes[0].failureSummary;
-  
-      //   const { description, help, helpUrl } = axeViolation;
-  
-      //   const resultObj = {
-      //     description,
-      //     help,
-      //     helpUrl,
-      //     whereItFailed,
-      //     // failureSummary,
-      //   }
-        
-      //   // console.log(resultObj);
-  
-      //   console.log('TEST DESCRIPTION: ', description,
-      //     '\nISSUE: ', help,
-      //     '\nMORE INFO: ', helpUrl,
-      //     '\nWHERE IT FAILED: ', whereItFailed,
-      //     // '\nhow to fix: ', failureSummary
-      //   )
-      // })
+      if (violations.length === 0) {
+        console.log('Congrats! Keep up the good work, you have 0 known violations!');
+        done();
+      }
       // console.log(results.violations);
       
+      violations.forEach((axeViolation) => {
+        console.log('-----------------------------------------------------line36-----------------------------------------------');
+        const whereItFailed = axeViolation.nodes[0].html;
+  
+        // [x] prints out the following
+        const failureSummary = axeViolation.nodes[0].failureSummary;
+  
+        const { description, help, helpUrl } = axeViolation;
+  
+        const resultObj = {
+          description,
+          help,
+          helpUrl,
+          whereItFailed,
+          // failureSummary,
+        }
+        
+        // console.log(resultObj);
+  
+        console.log('TEST DESCRIPTION: ', description,
+          '\nISSUE: ', help,
+          '\nMORE INFO: ', helpUrl,
+          '\nWHERE IT FAILED: ', whereItFailed,
+          // '\nhow to fix: ', failureSummary
+        )
+      })
+      
+      // console.log(results.violations);
+      done();
     });
   });
   
